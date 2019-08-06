93cfce5ee2c606671c5eba94206867b7bf50e44f
bit <bit@DESKTOP-TB3QSSE>
2019-08-06 16:16:38 +0900

    2번

--- src/practice01/prob2/Prob2.java
+++ src/practice01/prob2/Prob2.java
@@ -2,5 +2,16 @@ package practice01.prob2;
 
 public class Prob2 {
 	public static void main (String args[]) {
+		int i,j;
+		int k = 0;
+		
+		for(i=0; i<9; i++)
+		{
+			for(j=0; j<10; j++) {
+				k = i + j + 1;
+				System.out.print(k + " ");
+			}
+			System.out.println("");
+		}
 	}
 }
